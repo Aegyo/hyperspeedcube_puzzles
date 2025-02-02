be1bb8afd70b975c5ae5a304693dfa7f90650869
Jessica Chen <jessica.scoltock@protonmail.com>
2025-02-01 19:46:59 -0500

    Label pieces correctly

--- skewb_ultimate.lua
+++ skewb_ultimate.lua
@@ -1,5 +1,4 @@
-
-puzzles:add{
+puzzles:add {
     id = 'skewb_ultimate',
     name = "Skewb Ultimate",
     version = '1.0.0',
@@ -9,20 +8,23 @@ puzzles:add{
         local dodeca = lib.symmetries.h3.dodecahedron()
         local octa = lib.symmetries.bc3.octahedron()
 
-        local face_edge_angle = acos(-sqrt(1/(phi * sqrt(5))))
-        local adjust = rot{ fix = vec(1, 0, 0), angle = face_edge_angle}
+        local face_edge_angle = acos(-sqrt(1 / (phi * sqrt(5))))
+        local adjust = rot { fix = vec(1, 0, 0), angle = face_edge_angle }
 
-        for t, axis in dodeca:iter_poles() do
-            self:carve(adjust:transform(axis))
+        for t, axis, name in dodeca:iter_poles() do
+            self:carve(adjust:transform(axis), { stickers = self.colors[tostring(name)] })
         end
 
         self.axes:add(octa:iter_poles(), { INF, 0, -INF })
 
-        for t, axis, twist_t in octa.sym.chiral:orbit(self.axes[1], octa.sym:thru(3,2)) do
-            self.twists:add(axis, twist_t, { gizmo_pole_distance = 1/2 })
+        for t, axis, twist_t in octa.sym.chiral:orbit(self.axes[1], octa.sym:thru(3, 2)) do
+            self.twists:add(axis, twist_t, { gizmo_pole_distance = 1 / 2 })
         end
 
-        lib.piece_types.mark_everything_core(self)
+        lib.utils.unpack_named(_ENV, self.axes)
+        self:mark_piece(F(1) & R(1) & U(1) & L(1), 'edge', 'Edge')
+        self:mark_piece(D(1) & L(1) & R(1), 'corner', 'Corner')
+        self:unify_piece_types(octa.sym.chiral)
     end,
 
     tags = {
@@ -32,7 +34,6 @@ puzzles:add{
         'type/puzzle',
         'shape/3d/platonic/dodecahedron',
 
-        axes = { '3d/elementary/octahedral'},
+        axes = { '3d/elementary/octahedral' },
     },
 }
-
