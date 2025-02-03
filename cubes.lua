17c219fa87f33bfae1382e9fca0e43db6950e866
Jessica Chen <jessica.scoltock@protonmail.com>
2025-02-03 13:53:23 -0500

    Add ftrd

--- cubes.lua
+++ cubes.lua
@@ -15,3 +15,19 @@ puzzles:add{
         end
     end
 }
+
+puzzles:add{
+    id = 'face_turning_rhombic_dodecahedron',
+    name = 'Face Turning Rhombic Dodecahedron',
+    version = '0.1.0',
+    ndim = 3,
+    tags = { author = { 'Jessica Chen' } },
+    build = function(self)
+        local sym = cd'bc3'
+        self:carve(sym:orbit(sym.oxo.unit))
+        self.axes:add(sym:orbit(sym.oxo.unit), { INF, 2/3 })
+        for _, a, t in sym:orbit(self.axes[sym.oxo], sym:thru(3,1)) do
+            self.twists:add(a, t, { gizmo_pole_distance = 1 })
+        end
+    end
+}
