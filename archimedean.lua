59e83715b1a82cf0b9fd9a28941b9fb6f3356da0
Jessica Chen <jessica.scoltock@protonmail.com>
2025-02-04 12:35:10 -0500

    Fix truncated octahedron axis names, rotations, and piece names

--- archimedean.lua
+++ archimedean.lua
@@ -12,14 +12,38 @@ puzzles:add{
         self:carve(sym:orbit(sym.xoo.unit*hex_d))
         self:carve(sym:orbit(sym.oox.unit*square_d))
         
-        self.axes:add(sym:orbit(sym.xoo.unit), {INF, hex_d - 1/3})
-        self.axes:add(sym:orbit(sym.oox.unit), {INF, square_d - 1/4})
+        self.axes:add(sym:orbit(sym.xoo.unit):named({
+            BRU = {3, 'FRD'},
+            FLU = {1, 'FRU'},
+            BLU = {3, 'FLD'},
+            FRD = {2, 'FLU'},
+            FRU = {},
+            BRD = {2, 'BLU'},
+            FLD = {1, 'FRD'},
+            BLD = {1, 'BRD'},
+        }), {INF, hex_d - 1/3})
+        self.axes:add(sym:orbit(sym.oox.unit):named({
+            F = {},
+            U = {3, 'F'},
+            R = {2, 'U'},
+            L = {1, 'R'},
+            D = {2, 'L'},
+            B = {3, 'D'},
+          }), {INF, square_d - 1/4})
         
-        for _, a, t in sym:orbit(self.axes[sym.xoo], sym:thru(3,2)) do
+        for _, a, t in sym.chiral:orbit(self.axes[sym.xoo], sym:thru(3,2)) do
             self.twists:add(a, t, { gizmo_pole_distance = hex_d })
         end
-        for _, a, t in sym:orbit(self.axes[sym.oox], sym:thru(1,2)) do
+        for _, a, t in sym.chiral:orbit(self.axes[sym.oox], sym:thru(2,1)) do
             self.twists:add(a, t, { gizmo_pole_distance = square_d })
         end
+
+        lib.utils.unpack_named(_ENV, self.axes)
+        self:mark_piece(F(1) & ~FLU(1) & ~FLD(1) & ~FRU(1) & ~FRD(1), 'square_center', 'Square Center')
+        self:mark_piece(FLU(1) & ~F(1) & ~L(1) & ~U(1) & ~FRU(1) & ~FLD(1) & ~BLU(1), 'hex_center', 'Hexagon Center')
+        self:mark_piece(F(1) & FLU(1) & ~FLD(1) & ~FRU(1), 'square_edge', 'Square Edge')
+        self:mark_piece(FLU(1) & FRU(1) & ~F(1) & ~U(1), 'hex_edge', 'Hexagon Edge')
+        self:mark_piece(F(1) & FLU(1) & FRU(1), 'corner', 'Corner')
+        self:unify_piece_types(sym.chiral)
     end
 }
